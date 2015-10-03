8a715f36b4dd22f866d3b5349a9e8d4d6a4d56f0
satmn <kotubutokotora@gmail.com>
2015-10-03 18:45:54 +0900

    Revert "Revert "rename""
    This reverts commit 02a921c26b390b14303e284494ef3775e80d577a.

--- CMDB/CMDB/Program.cs
+++ CMDB/CMDB/Program.cs
@@ -15,7 +15,7 @@ namespace CMDB
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
